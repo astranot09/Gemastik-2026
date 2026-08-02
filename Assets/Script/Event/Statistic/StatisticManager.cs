cbe8d09ba3045071be86f6e7dde58e85ff6ab158
astranot09 <anthony.setiawan002@binus.ac.id>
2026-08-02 11:08:58 +0700

    statisticEvent

--- Assets/Script/Event/Statistic/StatisticManager.cs
+++ Assets/Script/Event/Statistic/StatisticManager.cs
@@ -1,16 +1,40 @@
+using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public enum StatisticEventType
+{
+    None,
+    Spicy,
+    Protein,
+    Healthy,
+    Warm,
+}
+[System.Serializable]
+public class StatisticListFood
+{
+    public StatisticEventType type;
+    public string nameEvent;
+    [TextArea (3,5)]public string descriptionEvent;
+    public List<MenuSO> menuSOs;
+}
 public class StatisticManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static StatisticManager instance;
+
+    private void Awake()
     {
-        
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+    public List<StatisticListFood> allStatisticEvent;
+
+    [SerializeField] private StatisticEventType currentEvent;
+    [SerializeField] private StatisticEventType currentEvent2;
+
+
 }
