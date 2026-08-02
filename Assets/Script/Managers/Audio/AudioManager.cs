d8e9d720b134864f19aff71a76c538f5cf4092b7
astranot09 <anthony.setiawan002@binus.ac.id>
2026-08-03 01:22:08 +0700

    Audio :D

---
+++ Assets/Script/Managers/Audio/AudioManager.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.Audio;
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+    }
+
+    [Header("Source")]
+    [SerializeField] private AudioSource BGM;
+    [SerializeField] private AudioSource SFX;
+
+    [Header("Clip BGM")]
+    [SerializeField] private AudioClip bgmClip;
+
+    [Header("Clip BGM")]
+    [SerializeField] private AudioClip dayFinished;
+
+
+    private void Start()
+    {
+        PlayBGM(bgmClip);
+    }
+
+    public void PlayBGM(AudioClip clip)
+    {
+        Debug.Log("music");
+        if(clip != null && BGM != null)
+        {
+            Debug.Log("play music");
+            BGM.clip = clip;
+            BGM.loop = true;
+            BGM.Play();
+        }
+    }
+    public void PlaySFX(AudioClip clip)
+    {
+        if (clip != null && SFX != null)
+        {
+            SFX.PlayOneShot(clip);
+        }
+    }
+
+}
