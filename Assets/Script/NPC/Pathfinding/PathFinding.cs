327ad16af3dde4aac21d0c48aae3f56c236c2632
Stopit-m8 <sunelsensurya@gmail.com>
2026-08-03 17:27:22 +0700

    p

---
+++ Assets/Script/NPC/Pathfinding/PathFinding.cs
@@ -0,0 +1,9 @@
+using NUnit.Framework;
+using UnityEngine;
+
+public class PathFinding : MonoBehaviour
+{
+    [SerializeField] private GameObject pointContainer;
+    private Transform[] points;
+    [SerializeField] private float speed;
+}
